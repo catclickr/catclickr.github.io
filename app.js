f11d0fe5052b2f75940b91d4c3be2ee047db05f2
codercolors <139995739+codercolors@users.noreply.github.com>
2023-07-21 10:37:44 -0600

    Red Dot

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
     // CATS
     let cats=0;
+    let cats_per_second=0;
+    let cats_per_click=1;
 
     // SHOP RELATED
     let items={
@@ -8,9 +10,13 @@ $(document).ready(function() {
             "count": 0,
             "price": 15
         },
-        "bird": {
+        "birdy": {
             "count": 0,
             "price": 100
+        },
+        "dot": {
+            "count": 0,
+            "price": 50,
         }
     }
     function set_shop() {
@@ -18,24 +24,28 @@ $(document).ready(function() {
         $("#mouse_price").text(`$${items["mouse"]["price"]}`);
         $("#mouse_count").text(`${items["mouse"]["count"]}`);
 
-        // BIRD
-        $("#bird_price").text(`$${items["bird"]["price"]}`);
-        $("#bird_count").text(`${items["bird"]["count"]}`);
+        // BIRDY
+        $("#birdy_price").text(`$${items["birdy"]["price"]}`);
+        $("#birdy_count").text(`${items["birdy"]["count"]}`);
+
+        // DOT
+        $("#dot_price").text(`$${items["dot"]["price"]}`);
+        $("#dot_count").text(`${items["dot"]["count"]}`);
     }
     set_shop();
 
     $(".update-shop").click(set_shop());
 
     item_interval("mouse", 1);
-    item_interval("bird", 5);
+    item_interval("birdy", 5);
 
     function item_interval(shop_item, cats_per_second) {
         $(`.${shop_item}`).click(function() {
-            console.log(cats);
+            items[shop_item]["count"]++;
             if (cats>=items[`${shop_item}`]["price"]) {
                 cats-=items[`${shop_item}`]["price"];
                 items[`${shop_item}`]["count"]++;
-                items[`${shop_item}`]["price"]+=Math.floor(items[`${shop_item}`]["price"]*1.7);
+                items[`${shop_item}`]["price"]+=Math.floor(items[`${shop_item}`]["price"]*1.6);
                 $(`#${shop_item}_price`).text(`$${items[`${shop_item}`]["price"]}`);
      
                 setInterval(function() {
@@ -48,11 +58,26 @@ $(document).ready(function() {
             }
         });
     }
+    
+    $(".dot").click(function() {
+        click_efficiency("dot", 2);
+    });
+
+    function click_efficiency(shop_item, multiplier) {
+        if (cats>=items[shop_item]["price"]) {
+            cats-=items[shop_item]["price"];
+            cats_per_click*=multiplier;
+            items[shop_item]["price"]*=3;
+            $("#dot_price").text(`$${items[shop_item]["price"]}`);
+        } else {
+            alert("Not enough cats!");
+        }
+    }
 
     // CHANGE CAT IMAGE
     $(".cat").mousedown(function() {
         $(this).attr("src", "pop_cat_open.jpg");
-        cats++;
+        cats+=cats_per_click;
         $("#cats").text(`Cats: ${cats}`);
     });
     $(".cat").mouseup(function() {
